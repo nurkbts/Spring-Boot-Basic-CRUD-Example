74f284df62a702ddf2a02819d9cdaa64c8ef9e3a
rotameraklisi <snrkabatas@hotmail.com>
2022-06-20 16:26:54 +0300

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud_springBoot.Repository;
-
-import com.example.crud_springBoot.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
